4630183dd2330bd9a7b28c209fa9b03379bb721f
Roland Bernard <rolli.bernard@gmail.com>
2022-05-26 21:29:14 +0200

    Added dynamic ast list

---
+++ src/ast/astlist.c
@@ -0,0 +1,75 @@
+
+#include "ast/ast.h"
+#include "util/alloc.h"
+
+#include "ast/astlist.h"
+
+#define INITIAL_LIST_CAPACITY 32
+
+DynamicAstList* createDynamicAstList() {
+    DynamicAstList* ret = ALLOC(DynamicAstList, 1);
+    initDynamicAstList(ret, 0);
+    return ret;
+}
+
+DynamicAstList* createDynamicAstListOfSize(size_t size) {
+    DynamicAstList* ret = ALLOC(DynamicAstList, 1);
+    initDynamicAstList(ret, size);
+    return ret;
+}
+
+void initDynamicAstList(DynamicAstList* list, size_t size) {
+    list->kind = AST_NONE;
+    list->nodes = ALLOC(AstNode*, size);
+    list->count = 0;
+    list->capacity = size;
+}
+
+void deinitDynamicAstList(DynamicAstList* list) {
+    for (size_t i = 0; i < list->count; i++) {
+        freeAstNode(list->nodes[i]);
+    }
+    FREE(list->nodes);
+}
+
+void freeDynamicAstList(DynamicAstList* list) {
+    deinitDynamicAstList(list);
+    FREE(list);
+}
+
+void addToDynamicAstList(DynamicAstList* list, AstNode* node) {
+    if (list->count == list->capacity) {
+        if (list->capacity == 0) {
+            list->capacity = INITIAL_LIST_CAPACITY;
+        } else {
+            list->capacity *= 2;
+        }
+        list->nodes = REALLOC(AstNode*, list->nodes, list->capacity);
+    }
+    list->nodes[list->count] = node;
+    list->count++;
+}
+
+void resizeDynamicAstList(DynamicAstList* list, size_t size) {
+    list->nodes = REALLOC(AstNode*, list->nodes, size);
+    if (list->count < size) {
+        list->count = size;
+    }
+    list->capacity = size;
+}
+
+void fitDynamicAstList(DynamicAstList* list) {
+    resizeDynamicAstList(list, list->count);
+}
+
+AstList* toStaticAstList(DynamicAstList* list) {
+    fitDynamicAstList(list);
+    return REALLOC(AstList, list, 1);
+}
+
+DynamicAstList* toDynamicAstList(AstList* list) {
+    DynamicAstList* ret = REALLOC(DynamicAstList, list, 1);
+    ret->capacity = ret->count;
+    return ret;
+}
+
---
+++ src/ast/astlist.h
@@ -0,0 +1,33 @@
+#ifndef _AST_ASTLIST_H_
+#define _AST_ASTLIST_H_
+
+#include "ast/ast.h"
+
+typedef struct {
+    AST_NODE_BASE
+    AstNode** nodes;
+    size_t count;
+    size_t capacity;
+} DynamicAstList;
+
+DynamicAstList* createDynamicAstList();
+
+DynamicAstList* createDynamicAstListOfSize(size_t size);
+
+void initDynamicAstList(DynamicAstList* list, size_t size);
+
+void deinitDynamicAstList(DynamicAstList* list);
+
+void freeDynamicAstList(DynamicAstList* list);
+
+void addToDynamicAstList(DynamicAstList* list, AstNode* node);
+
+void resizeDynamicAstList(DynamicAstList* list, size_t size);
+
+void fitDynamicAstList(DynamicAstList* list);
+
+AstList* toStaticAstList(DynamicAstList* list);
+
+DynamicAstList* toDynamicAstList(AstList* list);
+
+#endif
